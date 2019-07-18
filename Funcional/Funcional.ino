007e3b44f915c7cdc7d721b8a47d1fa535c3cbf7
MarulandaT <migue.marulanda99@gmail.com>
2019-07-18 12:51:43 -0500

    Modificaciones al codigo

--- Funcional/Funcional.ino
+++ Funcional/Funcional.ino
@@ -1,39 +1,43 @@
+//Llamado a librerias nececesarias
 #include "DHT.h"
 #include <LiquidCrystal.h> 
 #include <Servo.h>
 
+//Definición de parametros
 #define DHTPIN A1
 #define DHTYPE DHT11
 #define ledPin 22
 
+//Elementos a utilizar y sus puertos
 Servo myservo; 
 DHT dht(DHTPIN, DHTYPE);
-
 LiquidCrystal lcd(1, 2, 4, 5, 6, 7 );
 
+//Temperatura y humedad relativa para el invernadero
 float Temp = 30;
-float hum = 80;
-int pos_servo = 0; 
+float hum = 90;
 
 void setup() {
   // put your setup code here, to run once:
-  //Serial.begin(9600);
+  //Inicializacion de componentes
   lcd.begin(16,2);
-  //lcd.setCursor(0,0);
   dht.begin();
-  
   pinMode(ledPin,0);
   myservo.attach(24); 
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
+ 
+ //Parametros a leer desde el sensor
  float t = dht.readTemperature(); 
  float h = dht.readHumidity();
- 
-  myservo.write(0);
+
+ //Estados iniciales del led y serv
+  myservo.write(180);
   digitalWrite(ledPin, 0);
 
+//Imprimir informacion por pantalla
   lcd.setCursor(0,0);
   lcd.print("Temp: ");
   lcd.print(t,1);
@@ -43,30 +47,10 @@ void loop() {
   lcd.print(h,1);
   lcd.print(" %");
 
-  /*if (t>Temp){
-    digitalWrite(ledPin, 1);
-  } else {
-    if (t < Temp) {
-      digitalWrite(22, LOW);
-      digitalWrite(11, LOW);
-      //digitalWrite(2,LOW); 
-      digitalWrite(13, HIGH);
-    }
-    else {
-      if (h < hum) {
-        //myservo.write(90);
-        //delay(5000); 
-        //myservo.write(pos_servo);
-        
-      } else {
-        digitalWrite(2,LOW); 
-      }
-    }
-  }*/
-
+//Definicion de posibles casos entre temperatura y humedad para dar determinismo
 if(t > Temp && h < hum) {
   digitalWrite(ledPin, 1); 
-  myservo.write(90);
+  myservo.write(180);
   delay (2000);
   myservo.write(0);
 } else 
@@ -79,7 +63,7 @@ if(t > Temp && h < hum) {
    else 
     if (t < Temp && h < hum) {
       digitalWrite(ledPin, 0); 
-      myservo.write(90); 
+      myservo.write(180); 
       delay (2000);
       myservo.write(0);
     }
@@ -90,9 +74,7 @@ if(t > Temp && h < hum) {
       delay (2000);
       myservo.write(0);
     }
-  
-
-  
+    
   delay(1000);
   lcd.clear();
 }
